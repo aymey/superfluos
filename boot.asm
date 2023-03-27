b3a71e628f21a2067809a9977d54f46a5431f981
aymey <fabianpaci@gmail.com>
2023-03-28 06:58:14 +1100

    bios interrupt for reading disk

--- boot.asm
+++ boot.asm
@@ -2,10 +2,25 @@ ORG 0x7C00
 BITS 16
 
 start:
-    ; mov si, message
+    call readDisk
     call printPress
 
     jmp $
+
+readDisk:
+    mov ah, 2
+    mov al, 1
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov ax, 0x7C00
+    mov es, ax
+    mov bx, 0x7E00
+    int 0x13
+
+    mov ah, 0Eh
+    mov al, [0x7E00]
+    int 0x10
 
 printPress:
     mov ah, 0
@@ -17,3 +32,5 @@ printPress:
 
 times 510-($-$$) db 0
 dw 0xAA55
+
+times 512 db 'A'
