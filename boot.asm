fbbd06b720e3c4ceca55cd4a61e7ba5f1c5690cb
aymey <fabianpaci@gmail.com>
2023-03-30 20:08:51 +1100

    32 bit protected mode

--- boot.asm
+++ boot.asm
@@ -2,8 +2,13 @@ ORG 0x7C00
 BITS 16
 
 start:
-    call readDisk
-    call printPress
+    ; call readDisk
+    cli
+    lgdt [GDTDescriptor]
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+    jmp CODESEG:protectedMode
 
     jmp $
 
@@ -13,8 +18,9 @@ readDisk:
     mov ch, 0
     mov cl, 2
     mov dh, 0
-    mov ax, 0x7C00
-    mov es, ax
+    ; mov dl, 0
+    ; mov ax, 0
+    ; mov es, ax
     mov bx, 0x7E00
     int 0x13
 
@@ -22,15 +28,43 @@ readDisk:
     mov al, [0x7E00]
     int 0x10
 
-printPress:
-    mov ah, 0
-    int 0x16
+GDTStart:
+    nullDescriptor:
+        dd 0
+        dd 0
+    codeDescriptor:
+        dw 0xffff
+        dw 0
+        db 0
 
-    mov ah, 0Eh
-    int 0x10
-    call printPress
+        db 0b10011010
+        db 0b11001111
+
+        db 0
+
+    dataDescriptor:
+        dw 0xffff
+        dw 0
+        db 0
+
+        db 0b10010010
+        db 0b11001111
+
+        db 0
+GDTEnd:
+
+GDTDescriptor:
+    dw GDTEnd - GDTStart - 1
+    dd GDTStart
+
+    CODESEG equ codeDescriptor - GDTStart
+    DATASEG equ dataDescriptor - GDTStart
+
+[BITS 32]
+protectedMode:
+    mov al, 'A'
+    mov ah, 0x0f
+    mov [0xb8000], ax
 
 times 510-($-$$) db 0
 dw 0xAA55
-
-times 512 db 'A'
